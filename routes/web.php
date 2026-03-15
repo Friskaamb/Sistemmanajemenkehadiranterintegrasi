6328723a17a42b88576625365fc40e820b60e6ef
Friskaamb <oliviafa27@gmail.com>
2026-03-15 15:30:19 +0700

    MVC Attendance Management System

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,59 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AttendanceController;
 
-Route::get('/', function () {
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Praktikum List Barang (Routes → Controller → View)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+
+/*
+|--------------------------------------------------------------------------
+| PBL - Sistem Manajemen Kehadiran Karyawan
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/attendance', [AttendanceController::class, 'index']);
